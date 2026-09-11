5be6af8eea52aeccd8a3db6990c2035f216a304d
DorianLF <dleonfernandes@gmail.com>
2026-09-11 11:28:18 +0200

    wine

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from PIL import Image
 
 im=Image.open("images.jpg")
 #im.show()
-
+alpha = 0.25
 for i in range(im.size[0]):
-    im.putpixel((i,430),(250,10,10))
+    for j in range(im.size[1]//2, im.size[1]):
+        r, g, b = im.getpixel((i,j))
+        im.putpixel((i,j),(int(138*(1-alpha)+r*alpha), int(16*(1-alpha)+g*alpha), int(30*(1-alpha)+b*alpha)))
 
 im.save("images_modified.jpg")
 im.show()
